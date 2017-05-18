a03301f5aca7d0d0e0fe9f6fc65730582b404d23
Aaron Endsley <aaronendsley@gmail.com>
2017-05-18 18:52:08 -0500

    set up getting the value of the slect box and starting on getting the value of the age box

--- hhbuilder/index.js
+++ hhbuilder/index.js
@@ -1,1 +1,26 @@
-// Your code goes here
+(function init() {
+    var select = document.getElementsByTagName('select');
+    var inputAge = document.getElementsByName('age');
+    var errMessage;
+
+    inputAge.onblur = function getAge() {
+
+    }
+    select.rel.onchange = function checkSelectValue() {
+        var currentValue = select.rel.value;
+        if (select.rel.value) {
+            currentData.relationship = currentValue;
+        }
+        console.log(currentData);
+    }
+
+
+})();
+
+var currentData = {
+    age: 0,
+    relationship: '',
+    smoker: false
+}
+
+var submittedData = {};
