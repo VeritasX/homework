778763b63abda7f15b591faf106694f7f7204190
Aaron Endsley <aaronendsley@gmail.com>
2017-05-19 12:04:56 -0500

    added the function for submitting the data as well as clearing the form fields

--- hhbuilder/index.js
+++ hhbuilder/index.js
@@ -1,35 +1,64 @@
 (function init() {
-  var select = document.getElementsByTagName("select");
-  var inputAge = document.getElementsByName("age")[0];
-  var inputSmoking = document.getElementsByName("smoker")[0];
-  console.log(inputAge);
-  var errMessage;
+    var select = document.getElementsByTagName("select");
+    var inputAge = document.getElementsByName("age")[0];
+    var inputSmoking = document.getElementsByName("smoker")[0];
+    var addButton = document.querySelector('.add');
+    var divs = document.getElementsByTagName('div');
 
-  inputAge.onblur = function getAge() {
-    var age = inputAge.value;
-    if (!isNaN(age)) {
-      currentData.age = age;
+    addButton.type = 'button';
+
+    function resetCurrentData() {
+        currentData.age = 0;
+        currentData.rel = '';
+        currentData.smoker = false;
+        inputAge.value = '';
+        select.rel.value = '';
+        inputSmoking.checked = false;
     }
-    console.log(currentData);
-  };
-  select.rel.onchange = function checkSelectValue() {
-    var currentValue = select.rel.value;
-    if (select.rel.value) {
-      currentData.relationship = currentValue;
+
+    inputAge.onblur = function getAge() {
+        var age = parseInt(inputAge.value);
+        if (!isNaN(age) && age >= 1) {
+            currentData.age = age;
+        }
+        console.log(currentData);
+    };
+    select.rel.onchange = function checkSelectValue() {
+        var currentValue = select.rel.value;
+        if (select.rel.value) {
+            currentData.rel = currentValue;
+        }
+        console.log(currentData);
+    };
+    inputSmoking.onchange = function checkSmoking() {
+        var smokingIsTrue = inputSmoking.checked;
+        currentData.smoker = smokingIsTrue;
+        console.log(currentData);
+    };
+
+    addButton.onclick = function AddToSubmitedData() {
+        if (currentData.age > 0 && currentData.rel) {
+            var dataToPush = {
+                age: currentData.age,
+                rel: currentData.rel,
+                smoker: currentData.smoker
+            }
+            submittedData.push(dataToPush);
+            resetCurrentData();
+            console.log(submittedData);
+
+        }
+
     }
-    console.log(currentData);
-  };
-  inputSmoking.onchange = function checkSmoking() {
-    var smokingIsTrue = inputSmoking.checked;
-    currentData.smoker = smokingIsTrue;
-    console.log(currentData);
-  };
+
+
+
 })();
 
 var currentData = {
-  age: 0,
-  relationship: "",
-  smoker: false
+    age: 0,
+    rel: '',
+    smoker: false
 };
 
-var submittedData = {};
+var submittedData = [];
