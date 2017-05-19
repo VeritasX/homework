93323773ffd44c3f75d37f0f7c60441e96a8f4d2
Aaron Endsley <aaronendsley@gmail.com>
2017-05-18 21:36:41 -0500

    added an on change function to the smoker checkbox

--- hhbuilder/index.js
+++ hhbuilder/index.js
@@ -1,31 +1,35 @@
 (function init() {
-    var select = document.getElementsByTagName('select');
-    var inputAge = document.getElementsByName('age')[0];
-    console.log(inputAge);
-    var errMessage;
+  var select = document.getElementsByTagName("select");
+  var inputAge = document.getElementsByName("age")[0];
+  var inputSmoking = document.getElementsByName("smoker")[0];
+  console.log(inputAge);
+  var errMessage;
 
-    inputAge.onblur = function getAge() {
-        var age = inputAge.value;
-        if (!isNaN(age)) {
-            currentData.age = age;
-        }
-        console.log(currentData);
+  inputAge.onblur = function getAge() {
+    var age = inputAge.value;
+    if (!isNaN(age)) {
+      currentData.age = age;
     }
-    select.rel.onchange = function checkSelectValue() {
-        var currentValue = select.rel.value;
-        if (select.rel.value) {
-            currentData.relationship = currentValue;
-        }
-        console.log(currentData);
+    console.log(currentData);
+  };
+  select.rel.onchange = function checkSelectValue() {
+    var currentValue = select.rel.value;
+    if (select.rel.value) {
+      currentData.relationship = currentValue;
     }
-
-
+    console.log(currentData);
+  };
+  inputSmoking.onchange = function checkSmoking() {
+    var smokingIsTrue = inputSmoking.checked;
+    currentData.smoker = smokingIsTrue;
+    console.log(currentData);
+  };
 })();
 
 var currentData = {
-    age: 0,
-    relationship: '',
-    smoker: false
-}
+  age: 0,
+  relationship: "",
+  smoker: false
+};
 
 var submittedData = {};
