c92717240b35fccb73124bf283a30cf2525cec64
Aaron Endsley <aaronendsley@gmail.com>
2017-05-18 19:03:48 -0500

    setup the method for getting the value of the age field

--- hhbuilder/index.js
+++ hhbuilder/index.js
@@ -1,10 +1,15 @@
 (function init() {
     var select = document.getElementsByTagName('select');
-    var inputAge = document.getElementsByName('age');
+    var inputAge = document.getElementsByName('age')[0];
+    console.log(inputAge);
     var errMessage;
 
     inputAge.onblur = function getAge() {
-
+        var age = inputAge.value;
+        if (!isNaN(age)) {
+            currentData.age = age;
+        }
+        console.log(currentData);
     }
     select.rel.onchange = function checkSelectValue() {
         var currentValue = select.rel.value;
