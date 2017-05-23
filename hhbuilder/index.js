3984dc18e1b893635ac801d9e757cfc4725d439c
Aaron Endsley <aaronendsley@gmail.com>
2017-05-22 19:38:25 -0500

    added a function that displays the data to the screen as it is submitted

--- hhbuilder/index.js
+++ hhbuilder/index.js
@@ -3,9 +3,47 @@
         function validationInit(ageInput, select) {
             ageInput.innerHTML += '<p id="ageValidate" style="color:red;"></p>';
             select.innerHTML += '<p id="selectValidate" style="color:red;"></p>';
+
+        }
+
+        function checkSelf(arrayOfItems) {
+            var self = arrayOfItems.map(function (item) {
+                if (item.rel === 'self') {
+                    return true;
+                }
+
+            });
+            return self;
         }
         return {
-            validationInit: validationInit
+            validationInit: validationInit,
+            checkSelf: checkSelf
+        }
+    })();
+    var ui = (function uiMethods() {
+        function createComponent(age, relation, smoker) {
+            var ageValue = age,
+                relValue = relation,
+                smokeValue = smoker,
+                domElement = document.querySelector('.household');
+
+            if (smokeValue && relValue === 'self') {
+                smokeValue = 'Are You a smoker? Yes.';
+            } else if (!smokeValue && relValue === 'self') {
+                smokeValue = 'Are You a smoker? No.'
+            } else if (smokeValue && relValue !== 'self') {
+                smokeValue = 'Are they a smoker? Yes.';
+            } else if (!smokeValue && relValue !== 'self') {
+                smokeValue = 'Are they a smoker? No.';
+            }
+
+            domElement.innerHTML += '<ul><li>' + relation + '</li><li>' + age + '</li><li>' + smokeValue + '</li></ul>';
+
+
+        }
+
+        return {
+            createComponent: createComponent
         }
     })();
     var select = document.getElementsByTagName("select");
@@ -14,6 +52,9 @@
     var addButton = document.querySelector('.add');
     var selectBox = select.rel.parentElement.parentElement;
     var inputBox = inputAge.age.parentElement.parentElement;
+    var buttonBox = addButton.parentElement;
+    console.log(buttonBox);
+    addButton.type = 'button';
     validation.validationInit(inputBox, selectBox);
     console.log(inputAge)
 
@@ -50,21 +91,26 @@
         currentData.smoker = smokingIsTrue;
         console.log(currentData)
     };
-
+    console.log(addButton);
     addButton.onclick = function AddToSubmitedData() {
+
         if (currentData.age > 0 && currentData.rel) {
             var dataToPush = {
                 age: currentData.age,
                 rel: currentData.rel,
                 smoker: currentData.smoker
             }
+            ui.createComponent(dataToPush.age, dataToPush.rel,
+                dataToPush.smoker);
             submittedData.push(dataToPush);
             resetCurrentData();
             console.log(submittedData);
 
-        }
 
+
+        }
     }
+
 
 
 })();
